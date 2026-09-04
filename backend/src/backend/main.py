63a7253e1f2e80136c99db4b86aabad7934806ef
sujithkumar2506-prog <sujithkumar2506@gmail.com>
2026-09-04 11:29:52 +0530

    Upload file is saved

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -10,12 +10,28 @@ class Item(BaseModel):
 def home():
     return {'body':'Document processing API'}
 
-@app.post('/upload')
+
+@app.post("/upload")
 async def upload_document(myfile: UploadFile = File(...)):
-    print(f'Name of file is {myfile.content_type}')
-    # file = open(myfile,'r')
-    # content = file.read()
-    # print(content)
-    # file.close()
-   
-    return {'message':'Document is saved'}
+
+    print(f"Name of file is {myfile.filename}")
+    print(f"Content type is {myfile.content_type}")
+
+    content = await myfile.read()
+    content = content.decode("utf-8")
+    print(os.getcwd())
+    try:
+        with open(f"backend/uploads/{myfile.filename}", "a+") as file:
+            file.write(content)
+
+        return {
+            "message": "Document is saved",
+            "filename": myfile.filename
+        }
+
+    except Exception as e:
+        print(e)
+        return {
+            "response": 500,
+            "message": "Not saved"
+        }
