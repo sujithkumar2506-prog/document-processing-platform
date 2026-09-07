e369ee6c87a19baf0929b03de77fc8254eb0609f
sujithkumar2506-prog <sujithkumar2506@gmail.com>
2026-09-07 16:19:08 +0530

    Reliable file upload feature added

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,7 +1,8 @@
+import uuid
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from pathlib import Path
-UPLOAD_DIR = Path('backend/uploads')
+UPLOAD_DIR = Path('uploads')
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 app = FastAPI()
@@ -15,13 +16,16 @@ def home():
 @app.post("/upload")
 async def upload_document(myfile: UploadFile = File(...)):
 
-    filename = myfile.filename
+    filename = Path(myfile.filename).name
     file_type = myfile.content_type
 
     content = await myfile.read()
     
     # print(os.getcwd())
-    file_path = UPLOAD_DIR/filename
+    unique_id = uuid.uuid4()
+    unique_file_name = f"{unique_id}_{filename}"
+    print(unique_id,unique_file_name)
+    file_path = UPLOAD_DIR/unique_file_name
     try:
         with open(file_path, "wb") as file:
             file.write(content)
